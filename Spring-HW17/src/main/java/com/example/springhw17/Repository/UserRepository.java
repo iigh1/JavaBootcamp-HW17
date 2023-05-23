21c6f9bc2f1127b35124afd55f6506602156fb22
Ghadeer <102277108+iigh1@users.noreply.github.com>
2023-05-23 19:06:09 +0300

    Delete Spring-HW17 directory

--- Spring-HW17/src/main/java/com/example/springhw17/Repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.springhw17.Repository;
-
-import com.example.springhw17.Model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends JpaRepository<User,Integer> {
-}
